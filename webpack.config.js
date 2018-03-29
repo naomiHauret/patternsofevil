1a333cb87e9cd2a561fe74ff87e9315a0deb41b9
naomiHauret <hauretnaomi@gmail.com>
2018-03-29 18:21:42 +0200

    :bug: fixed webpack bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,7 @@
 const path = require("path");
-const webpack = require("webpack");
-const HtmlPlugin = require("html-webpack-plugin");
+const webpack = require("webpack")
+const HtmlPlugin = require("html-webpack-plugin")
 const exludedFolders = [path.join(__dirname, "node_modules")];
-
-let htmlPluginConfig;
-
-process.env.NODE_ENV === "production"
-  ? (htmlPluginConfig = {
-      template: "./src/index.html",
-      excludeChunks: ["base"],
-      filename: "index.html",
-      minify: {
-        collapseWhitespace: true,
-        collapseInlineTagWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-      }
-    })
-  : {
-      template: "./src/index.html",
-      filename: "index.html",
-    }
-;
 
 module.exports = {
   entry: ["./src/index.js"],
@@ -34,7 +14,7 @@ module.exports = {
   },
   output: {
     filename: "assets/scripts/[name].[hash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
@@ -42,14 +22,24 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: exludedFolders,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
   plugins: [
-    new HtmlPlugin(htmlPluginConfig),
+    new HtmlPlugin({
+      template: "./src/index.html",
+      excludeChunks: ["base"],
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
+    })
   ],
   resolve: {
-    modules: [path.resolve("./src"), path.resolve("./node_modules")],
+    modules: [path.resolve("./src"), path.resolve("./node_modules")]
   }
-};
+}
