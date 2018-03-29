b2e7633b518564e9643a05f6b38ab97a4ca2f02f
naomiHauret <hauretnaomi@gmail.com>
2018-03-29 18:17:36 +0200

    💬  added more comments to code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "index.css";
 import dat from "dat.gui";
-import C2S from "canvas2svg"
+import C2S from "canvas2svg";
 
 const gui = new dat.GUI();
 const body = document.body;
@@ -8,7 +8,7 @@ const spiralCanvas = document.getElementById("spiral");
 const spiralCtx = spiralCanvas.getContext("2d");
 const imgCtx = document.getElementById("image").getContext("2d");
 const downloadButton = document.querySelector("#downloadSvgCode");
-let svgExportCtx
+let svgExportCtx;
 
 const params = {
   steps: 35000,
@@ -23,11 +23,12 @@ const params = {
   isFilled: true,
 };
 
-spiralCanvas.style.backgroundColor = params.backgroundColor
+spiralCanvas.style.backgroundColor = params.backgroundColor;
 
+// Dat.gui params
 gui.add(params, "steps").onChange((newValue) => {
-  params.steps = newValue
-  redraw()
+  params.steps = newValue;
+  redraw();
 });
 
 gui.add(params, "maxThick").onChange(newValue => {
@@ -91,42 +92,48 @@ const processImg = (img, ctx, params) => {
 
   // Map dimensions
   let scaleF = sizes.map(e => e[0] / e[1]).reduce((a, b) => Math.min(a, b));
-  let scaledDims = imgSize.map(e => e / scaleF);
-  let imgPos = zip(scaledDims, canvasSize, (a, b) => -(a - b) / 2);
-  ctx.drawImage(img, 0, 0, ...imgSize, ...imgPos, ...scaledDims); // first we draw our image
+  let scaledDims = imgSize.map(e => e / scaleF); // resize image so it fits in our canvas
+  let imgPos = zip(scaledDims, canvasSize, (a, b) => -(a - b) / 2);  // center our image in the middle of our canvas
+  ctx.drawImage(img, 0, 0, ...imgSize, ...imgPos, ...scaledDims); // first we draw our image to its canvas
 
   drawSpiral(spiralCtx, imgCtx, params); // then, we draw our spiral based on this image
 }
 
 const drawSpiral = (spiralCtx, imgCtx, params) => {
+  // Initialize and the parameters of the drawn spiral to dat.gui
   let steps = params.steps;
   let maxThick = params.maxThick;
   let minThick = params.minThick;
   let wig = params.wiggleWaviness;
-  let centerWidth = params.centerWidth;  // center of the spiral width
+  let centerWidth = params.centerWidth;  // size of the "void" at the beggining of the spiral
   let wiggleDistance = params.wiggleDistance; // distance between two "wiggles"
   let centerx = spiralCtx.canvas.width / 2;
   let centery = spiralCtx.canvas.height / 2;
-  svgExportCtx = new C2S(spiralCtx.canvas.width, spiralCtx.canvas.height);
+
+  svgExportCtx = new C2S(spiralCtx.canvas.width, spiralCtx.canvas.height); // for our SVG export
 
   // let's spiraaaaaal
-  spiralCtx.clearRect(0, 0, spiralCtx.canvas.width, spiralCtx.canvas.height);  
+  spiralCtx.clearRect(0, 0, spiralCtx.canvas.width, spiralCtx.canvas.height); // wipe out canvas
   svgExportCtx.clearRect(0, 0, spiralCtx.canvas.width, spiralCtx.canvas.height);
 
   for (let i = 0; i < steps; i++) {
     let angle = params.spiralRadius / steps * i;
-    let x = centerx + (centerWidth + wiggleDistance * angle) * Math.cos(angle) + Math.random() * wig;
-    let y = centery + (centerWidth + wiggleDistance * angle) * Math.sin(angle) + Math.random() * wig;
+    let x = centerx + (centerWidth + wiggleDistance * angle) * Math.cos(angle) + Math.random() * wig; // x position of the area we're going to redraw
+    let y = centery + (centerWidth + wiggleDistance * angle) * Math.sin(angle) + Math.random() * wig; // y position of the area we're going to redraw
 
-    let pxl = imgCtx.getImageData(x / 2, y / 2, 1, 1).data.slice(0, 3); // RGB value
-    let pxlB = 255 - pxl.reduce((centerWidth, wiggleDistance) => centerWidth + wiggleDistance) / 3; // set pixel color to black or white depending on its color
+    let pxl = imgCtx.getImageData(x/2, y/2, 1, 1).data.slice(0, 3); // get RGB value of of a pixel at position x/2, y/2
+    let pxlB = 255 - pxl.reduce((centerWidth, wiggleDistance) => centerWidth + wiggleDistance) / 3; // set pixel color to color or transparent depending on its RGB value
 
-		let h = minThick + pxlB / (255 / (maxThick - minThick));
+		let h = minThick + pxlB / (255 / (maxThick - minThick)); // dimension
 
-    if(params.isFilled === true) {
+    spiralCtx.strokeWidth = 1;
+    svgExportCtx.strokeWidth = 1;
+
+    // draw area
+    if(params.isFilled === true) { // fill the area with color
       spiralCtx.fillRect(x, y, h, h);
       svgExportCtx.fillRect(x, y, h, h);
-    } else {
+    } else { // just draw the border of the area
       spiralCtx.strokeRect(x, y, h, h);
       svgExportCtx.strokeRect(x, y, h, h);
     }
@@ -139,15 +146,18 @@ const preventDefault = event => {
   event.preventDefault();
 };
 
+// Draw image to the canvas after we drop it
 const handleDrop = event => {
   event.preventDefault();
   let reader = new window.FileReader();
+
   reader.onload = event => {
     img = new Image();
     img.src = event.target.result;
     lastImg = img;
     img.onload = () => processImg(img, imgCtx, params);
   };
+
   let file = event.dataTransfer.files[0];
   reader.readAsDataURL(file);
 };
@@ -165,7 +175,6 @@ const download = (filename, content) => {
   document.body.removeChild(pseudoLink);
 }
 
-
 downloadButton.addEventListener(
   "click", () => {
     let svg = svgExportCtx.getSerializedSvg();
@@ -175,6 +184,7 @@ downloadButton.addEventListener(
   },
   false
 );
+
 window.addEventListener("drop", preventDefault);
 window.addEventListener("dragover", preventDefault);
 body.addEventListener("drop", handleDrop);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
 const path = require("path");
-const webpack = require("webpack")
-const HtmlPlugin = require("html-webpack-plugin")
+const webpack = require("webpack");
+const HtmlPlugin = require("html-webpack-plugin");
 const exludedFolders = [path.join(__dirname, "node_modules")];
+
+let htmlPluginConfig;
+
+process.env.NODE_ENV === "production"
+  ? (htmlPluginConfig = {
+      template: "./src/index.html",
+      excludeChunks: ["base"],
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+      }
+    })
+  : {
+      template: "./src/index.html",
+      filename: "index.html",
+    }
+;
 
 module.exports = {
   entry: ["./src/index.js"],
@@ -14,7 +34,7 @@ module.exports = {
   },
   output: {
     filename: "assets/scripts/[name].[hash].js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -22,27 +42,14 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: exludedFolders,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       }
     ]
   },
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
-    }),
-    new HtmlPlugin({
-      template: "./src/index.html",
-      excludeChunks: ["base"],
-      filename: "index.html",
-      minify: {
-        collapseWhitespace: true,
-        collapseInlineTagWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true
-      }
-    })
+    new HtmlPlugin(htmlPluginConfig),
   ],
   resolve: {
-    modules: [path.resolve("./src"), path.resolve("./node_modules")]
+    modules: [path.resolve("./src"), path.resolve("./node_modules")],
   }
-}
+};
